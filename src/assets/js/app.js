4982fdd4226bcdc9689470fdd89def74f31eed1e
Tolubaria <85737416+Tolubaria@users.noreply.github.com>
2021-10-26 12:04:11 +0300

    validation

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -1,1 +1,29 @@
-console.log('file 1');
+let iconPassword = document.querySelectorAll(".icon-password");
+
+iconPassword.forEach(item => item.addEventListener('click', toggleType));
+
+function toggleType() {
+    let input = this.closest(".input-wrapper").querySelector(".password");
+    this.classList.remove("fa-eye");
+    this.classList.add("fa-eye-slash");
+    if (input.type == "password") {
+        input.type = "text";
+    } else {
+        input.type = "password";
+    }
+}
+
+let btn = document.querySelector(".btn");
+let toIntroduce = document.querySelector(".to-introduce");
+let toConfirm = document.querySelector(".to-confirm");
+
+btn.onclick = function() {
+    if (toIntroduce.value == toConfirm.value) {
+        alert("You are welcome")
+    } 
+    else {
+        document.querySelector(".password-form").insertAdjacentHTML("beforeend", `<span class="alert" style='color:red'>
+            Нужно ввести одинаковые значения.
+            </span>`);
+    }
+};
